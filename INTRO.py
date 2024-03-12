039a65515d339d07ba1153dc0508c82b0ea72892
iankamau29 <iankamau29@gmail.com>
2024-03-12 13:36:42 +0300

    second commit for python

---
+++ INTRO.py
@@ -0,0 +1,51 @@
+# python comments-this is a single line comment
+"""
+this is a sample more than one line comments
+
+
+"""
+student_name = "ian"
+student_age = "30"
+is_male = True
+print(student_name)
+print(student_age)
+print(is_male)
+
+# outputting the values in the variables
+student_name = "ryan"
+student_name = "ian"
+print(student_name)
+
+STUDENT_NAME = "ryan"
+student_name = "ian"
+print(STUDENT_NAME)
+X = Y = Z = 10.0
+X, Y, Z = 10, 11, 12
+x= int(1)
+y= int(2.8)
+
+price=10
+qty=7
+total=price*qty
+myanswer=" Total :"+str(total)+"kenyan shillings"
+myanswer1="the total is:",total,"kenyan shillings"
+print(myanswer)
+
+a=1
+b="2"
+c=a+int(b)
+print(c)
+
+
+
+
+
+
+age=29
+residence ="kitengela"
+
+if residence=="nairobi" or residence=="kasarani":
+    print("can vie for Governor of Nairobi")
+else:
+    print("Do not meet requirements to become Governor of Nairobi")
+
