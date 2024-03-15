cf552f87d9761c4d595d3d5a6766c1d8417bc91b
iankamau29 <iankamau29@gmail.com>
2024-03-15 11:23:33 +0300

    third commit fot python

--- INTRO.py
+++ INTRO.py
@@ -21,31 +21,112 @@ student_name = "ian"
 print(STUDENT_NAME)
 X = Y = Z = 10.0
 X, Y, Z = 10, 11, 12
-x= int(1)
-y= int(2.8)
+x = int(1)
+y = int(2.8)
 
-price=10
-qty=7
-total=price*qty
-myanswer=" Total :"+str(total)+"kenyan shillings"
-myanswer1="the total is:",total,"kenyan shillings"
+price = 10
+qty = 7
+total = price * qty
+myanswer = " Total :" + str(total) + "kenyan shillings"
+myanswer1 = "the total is:", total, "kenyan shillings"
 print(myanswer)
 
-a=1
-b="2"
-c=a+int(b)
+a = 1
+b = "2"
+c = a + int(b)
 print(c)
 
+pens = 60.0
+books = "50"
+total = pens + float(books)
+result = "My Total is   :$" + str(total) + "kenyan shillings"
+print(result)
 
+age = 29
+residence = "nairobi"
 
-
-
-
-age=29
-residence ="kitengela"
-
-if residence=="nairobi" or residence=="kasarani":
+if residence == "nairobi" or residence == "kasarani":
     print("can vie for Governor of Nairobi")
 else:
     print("Do not meet requirements to become Governor of Nairobi")
+
+# x=int(input("enter number to be checked:"))
+
+"""
+#loops
+#the while loop
+x=1
+while x<=5:
+    if x==3:
+        break
+    print(x)
+    x+=1
+"""
+# the continue statement
+i = 0
+while i < 6:
+    if i == 3:
+        i += 1
+        continue
+    print(i)
+    i += 1
+""""
+countyr=input("enter country:")
+if countyr == "Rwanda":
+    print("East Africa")
+elif countyr=="Kenya":
+    print("East Africa")
+elif countyr == "Tanzania":
+    print("East Africa")
+elif countyr == "Rwanda":
+    print("East Africa")
+else:
+    print("unknown country")
+    """
+x=1
+while x<=5:
+    print("the number is:",x)
+    x+=1
+else:
+    print("loop ended")
+i=1
+while i < 10:
+        if i == 3:
+            i += 1
+            continue
+        print(i)
+        i += 1
+        if i == 5:
+            i += 1
+            continue
+
+x=1
+while x<=100:
+    print("the number is:",x)
+    x+=2
+total=("x+=x")
+print("total")
+
+def sum_of_odd_numbers(n):
+    total = 0
+    for i in range(1, n+1, 2):  # Start from 1 and step by 2 to cover only odd numbers
+        total += i
+    return total
+
+# Example usage:
+n = 100  # Sum odd numbers up to 10
+result = sum_of_odd_numbers(n)
+print("Sum of odd numbers up to", n, "is:", result)
+
+number=input("number of peole visiting:")
+kenyans=input("number of kenyans")
+rwandise=input("number of rwandise")
+
+
+
+
+
+
+
+
 
